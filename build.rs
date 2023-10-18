acb53d9de2ab5c992e152cec560de9fd6a14a15e
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2023-10-17 17:23:28 -0700

    Modified build.rs to create wrappers for static inlined functions, so that they are discoverable from the .so library.

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
+mod create_inlined_wrappers;
 use std::io::Write;
 
 extern crate bindgen;
-
 fn main(){
+    
     let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let src_path = out_path.join("libfabric");
     let build_path = src_path.join("build");
@@ -10,11 +11,12 @@ fn main(){
         Ok(val) => val,
         Err(_) => "".to_string(),
     };
-
+    
     let libfabrics_path;
     if custom_path == ""{
-
+        
         libfabrics_path = build_path.to_str().unwrap().to_owned();
+        // println!("cargo:rerun-if-changed={}", libfabrics_path.clone()+"/include/fabric_sys.h");
         
         // If ./libfabric does not exist we need to fetch it first
         if !std::path::Path::new(&(out_path.to_str().unwrap().to_owned()+ "/libfabric")).exists(){
@@ -39,28 +41,73 @@ fn main(){
     else{
 
         libfabrics_path = custom_path;
+        // println!("cargo:rerun-if-changed={}", libfabrics_path.clone()+"/include/fabric_sys.h");
     }
     
-    let headers = std::fs::read_dir(libfabrics_path.clone()+"/include/rdma/").unwrap();
+    // libfabric has multiple header file that we are required to generate bindings for so we create a
+    // new header named fabric_sys.h and #include all headers there
     let header_file = std::fs::File::create(libfabrics_path.clone()+"/include/fabric_sys.h").unwrap();
     let mut writer = std::io::BufWriter::new(header_file);
+    
+    // Another problem is that there are several static inline functions in these header files which do
+    // not create symbols in the libfabric.so and thus cannot be linked from rust.
+    // For this reason, we create wrapper functions for each inline one and store them in a file called inlined.c
+    // which resides in the /build/ directory 
+    let inlined_file = std::fs::File::create(libfabrics_path.clone()+"/inlined.c").unwrap();
+    let mut writer_inlined = std::io::BufWriter::new(inlined_file);
+    let _ = writer_inlined.write_all(b"#include<fabric_sys.h>\n");
+    
+    // We keep the prototypes of all the wrappers fo rinline functions here and will append them to fabric_sys.h
+    // so that they are visible to bindgen
+    let mut inlined_funcs: Vec<String> = Vec::new();
+
+    let headers = std::fs::read_dir(libfabrics_path.clone()+"/include/rdma/").unwrap();
+    
+    // For each file in libfabric/include/
     for header in headers {
         if header.as_ref().unwrap().file_type().unwrap().is_file() {
+            // Create the wrappers (prototype, definition) for all inlined functions
+            let mut funcs = crate::create_inlined_wrappers::read_file(header.as_ref().unwrap().path().to_str().unwrap());
+            
+            // Store the prototype for later
+            inlined_funcs.append(&mut funcs.0);
+            
+            // Write the definition to inlined.c
+            for f in funcs.1 {
+                let _ = writer_inlined.write_all((f+"\n").as_bytes());
+            }
+
+            // #include the header to fabric_sys.h
             let _ = writer.write_all(("#include<rdma/".to_owned()+&header.as_ref().unwrap().file_name().into_string().unwrap()+">\n").as_bytes());
         }
     }
+
+    // Append the prototypes of the wrappers for the inline functions to fabric_sys.h
+    for f in inlined_funcs {
+        let _ = writer.write_all((f+"\n").as_bytes());
+    }
+
+    // Make sure the files are written completely
     let _ = writer.flush();
+    let _ = writer_inlined.flush();
+
+    // Create a new lib, libinlined.so that we expose the wrappers for rust to use
+    std::process::Command::new("gcc").current_dir(libfabrics_path.clone() +"/lib/").arg("-Wno-everything").arg("-O3").arg("-I../include/").arg("-fPIC").arg("--shared").arg("-o").arg("libinlined.so").arg("../inlined.c").status().unwrap();
+    
+    // Generate the rust bindings
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}",libfabrics_path.clone()+"/include/"))
         .header(libfabrics_path.clone()+"/include/fabric_sys.h")
         .generate()
         .expect("Unable to generate bindings");
 
+    // Write them to the respective file
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
     
+    // Link with the libfabric and libinlined libraries to access their symbols. 
     println!("cargo:rustc-link-search={}", libfabrics_path.clone() +"/lib/");
     println!("cargo:rustc-link-lib=fabric");
-
+    println!("cargo:rustc-link-lib=inlined");
 }
---
+++ create_inlined_wrappers.rs
@@ -0,0 +1,79 @@
+use std::io::BufRead;
+
+fn read_lines<P>(filename: P) -> std::io::Result<std::io::Lines<std::io::BufReader<std::fs::File>>> where P: AsRef<std::path::Path>, {
+    let file = std::fs::File::open(filename)?;
+    Ok(std::io::BufReader::new(file).lines())
+}
+
+pub fn read_file(filepath: &str) -> (Vec<String>, Vec<String>){
+    let mut inlined_funcs_proto = Vec::new();
+    let mut inlined_funcs_impl = Vec::new();
+    if let Ok(lines) = read_lines(filepath) {
+        let mut keep = false;
+        let mut curr_func = String::new();
+        for oline in lines {
+            if let Ok(line) = oline {
+                if line.starts_with("static inline") {
+                    keep = true;
+                    if let Some(idx) = line.find("{") {
+                        curr_func.push_str(&line[13..idx]);
+                        curr_func.push_str(";");
+                    }
+                    else {
+                        curr_func.push_str(&line[13..]);
+                    }
+                }
+                else if keep {
+                    if let Some(idx) = line.find("{") {
+                        if ! line.starts_with("{") {
+                            curr_func.push_str(" ");
+                        }
+                        curr_func.push_str(&line[0..idx].trim());
+                        curr_func.push_str(";");
+                        let res = generate_wrapper_proto_and_body(&curr_func);
+                        inlined_funcs_proto.push(res.0);
+                        inlined_funcs_impl.push(res.1);
+                        curr_func = String::new();
+                        keep = false;
+                    }
+                    else {
+                        curr_func.push_str(" ");
+                        curr_func.push_str(&line.trim());
+                    }
+                }
+            }
+        }
+    }
+
+    return (inlined_funcs_proto, inlined_funcs_impl);
+}
+
+pub fn generate_wrapper_proto_and_body(func_proto: &str) -> (String, String){
+    let lpos = func_proto.find("(").unwrap() + 1;
+    let rpos = func_proto.find(")").unwrap();
+    let mut wrapper_proto = func_proto[..func_proto.len()-1].trim().to_string();
+    let mut wrapper_impl = String::new();
+    if let Some(name_pos) =  wrapper_proto[0..lpos].rfind("fi_") {
+        wrapper_proto.insert_str(name_pos, "inlined_");
+        wrapper_impl.push_str(&wrapper_proto);
+        wrapper_proto.push(';');
+        wrapper_impl.push_str("\n{\n");
+        wrapper_impl.push_str("\treturn ");
+        wrapper_impl.push_str(&func_proto[name_pos..lpos-1].replace("*", ""));
+        let args = func_proto[lpos..rpos].split(',').map(|x| x.split(" ").last().unwrap().replace("*", "").replace("void", "")).collect::<Vec::<String>>().join(",");
+        wrapper_impl.push_str(&format!("({});", args));
+        wrapper_impl.push_str("\n}\n");
+    }
+    
+
+    return (wrapper_proto, wrapper_impl);
+}
+
+// fn main() {
+
+//     let inlined_funcs = read_file("target/release/build/libfabrics-sys-58d2aac918303fbf/out/libfabric/build/include/rdma/fi_atomic.h");
+//     println!("Found {} inlined functions\n", inlined_funcs.0.len());
+//     for f in std::iter::zip(inlined_funcs.0, inlined_funcs.1) {
+//         println!("{}\n{}", f.0, f.1);
+//     }
+// }
