72a2c3603553abedafa8ae43d9785023f07a1d70
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2024-01-28 21:48:13 -0800

    Pinned libfabric submodule to specific commit

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ extern crate bindgen;
 fn main(){
     
     let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let src_path = out_path.join("libfabric");
+    let src_path = std::fs::canonicalize(std::path::PathBuf::from("../libfabric-c")).unwrap();
     let build_path = src_path.join("build");
     let custom_path = match std::env::var("LIBFABRIC_ROOT") {
         Ok(val) => val,
@@ -19,19 +19,19 @@ fn main(){
         // println!("cargo:rerun-if-changed={}", libfabrics_path.clone()+"/include/fabric_sys.h");
         
         // If ./libfabric does not exist we need to fetch it first
-        if !std::path::Path::new(&(out_path.to_str().unwrap().to_owned()+ "/libfabric")).exists(){
+        // if !std::path::Path::new(&(out_path.to_str().unwrap().to_owned()+ "/libfabric")).exists(){
     
-            std::process::Command::new("git").arg("clone").arg("https://github.com/ofiwg/libfabric.git").arg(src_path.to_str().unwrap()).status().unwrap();
-            std::process::Command::new("git").current_dir(src_path.to_str().unwrap()).arg("fetch").arg("--tags").status().unwrap();
-            std::process::Command::new("git").current_dir(src_path.to_str().unwrap()).arg("checkout").arg("tags/v1.19.0").status().unwrap();
-        }
+        //     std::process::Command::new("git").arg("clone").arg("https://github.com/ofiwg/libfabric.git").arg(src_path.to_str().unwrap()).status().unwrap();
+        //     std::process::Command::new("git").current_dir(src_path.to_str().unwrap()).arg("fetch").arg("--tags").status().unwrap();
+        //     std::process::Command::new("git").current_dir(src_path.to_str().unwrap()).arg("checkout").arg("tags/v1.19.0").status().unwrap();
+        // }
     
     
         // If /libfabric/build does not exist we have not built libfabric yet
-        if !std::path::Path::new(&(out_path.to_str().unwrap().to_owned()+ "/libfabric/build")).exists(){
+        if !build_path.exists(){
     
             std::process::Command::new("sh").current_dir(src_path.to_str().unwrap()).arg("autogen.sh").status().unwrap();
-            std::process::Command::new("sh").current_dir(src_path.to_str().unwrap()).arg("configure").arg("--prefix=".to_owned()+build_path.to_str().unwrap()).status().unwrap();
+            std::process::Command::new("sh").current_dir(src_path.to_str().unwrap()).arg("configure").arg("--prefix=".to_owned()+build_path.to_str().arg("--enable-static").unwrap()).status().unwrap();
         }
         
         
@@ -46,14 +46,14 @@ fn main(){
     
     // libfabric has multiple header file that we are required to generate bindings for so we create a
     // new header named fabric_sys.h and #include all headers there
-    let header_file = std::fs::File::create(libfabrics_path.clone()+"/include/fabric_sys.h").unwrap();
+    let header_file = std::fs::File::create(out_path.to_str().unwrap().to_owned()+"/fabric_sys.h").unwrap();
     let mut writer = std::io::BufWriter::new(header_file);
     
     // Another problem is that there are several static inline functions in these header files which do
     // not create symbols in the libfabric.so and thus cannot be linked from rust.
     // For this reason, we create wrapper functions for each inline one and store them in a file called inlined.c
     // which resides in the /build/ directory 
-    let inlined_file = std::fs::File::create(libfabrics_path.clone()+"/inlined.c").unwrap();
+    let inlined_file = std::fs::File::create(out_path.to_str().unwrap().to_owned()+"/inlined.c").unwrap();
     let mut writer_inlined = std::io::BufWriter::new(inlined_file);
     let _ = writer_inlined.write_all(b"#include<fabric_sys.h>\n");
     
@@ -92,12 +92,12 @@ fn main(){
     let _ = writer_inlined.flush();
 
     // Create a new lib, libinlined.so that we expose the wrappers for rust to use
-    std::process::Command::new("gcc").current_dir(libfabrics_path.clone() +"/lib/").arg("-Wno-everything").arg("-O3").arg("-I../include/").arg("-fPIC").arg("--shared").arg("-o").arg("libinlined.so").arg("../inlined.c").status().unwrap();
+    std::process::Command::new("gcc").current_dir(out_path.to_str().unwrap().to_owned()).arg("-Wno-everything").arg("-O3").arg("-I".to_owned()+&libfabrics_path+"../include/").arg("-fPIC").arg("--shared").arg("-o").arg("libinlined.so").arg(out_path.to_str().unwrap().to_owned()+"/inlined.c").status().unwrap();
     
     // Generate the rust bindings
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}",libfabrics_path.clone()+"/include/"))
-        .header(libfabrics_path.clone()+"/include/fabric_sys.h")
+        .header(out_path.to_str().unwrap().to_owned()+"/fabric_sys.h")
         .generate()
         .expect("Unable to generate bindings");
 
@@ -108,6 +108,7 @@ fn main(){
     
     // Link with the libfabric and libinlined libraries to access their symbols. 
     println!("cargo:rustc-link-search={}", libfabrics_path.clone() +"/lib/");
+    println!("cargo:rustc-link-search={}", out_path.to_str().unwrap().to_owned());
     println!("cargo:rustc-link-lib=fabric");
     println!("cargo:rustc-link-lib=inlined");
 }
