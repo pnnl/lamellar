ecfa8a896862da7e30ea00c23d1ad53a47e76bb4
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2024-01-31 16:07:21 -0800

    Added pmi2-sys

---
+++ build.rs
@@ -0,0 +1,26 @@
+
+extern crate bindgen;
+fn main(){
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let src_inc_path = std::path::PathBuf::from(std::env::var("PMI2_INC_DIR").expect("PMI2 not found. Please provide path to PMI2 include dir in \"PMI2_INC_DIR\" environmental variable"));
+    let src_lib_path = std::path::PathBuf::from(std::env::var("PMI2_LIB_DIR").expect("PMI2 not found. Please provide path to PMI lib dir in \"PMI2_LIB_DIR\" environmental variable"));
+
+    // let header_path = src_inc_path.join("/pmi2.h");
+    // let lib_path = src_lib_path.join("/lib/");
+
+    // Generate the rust bindings
+    let bindings = bindgen::Builder::default()
+        // .clang_arg(format!("-I{}",header_pathlude/"))
+        .header(src_inc_path.as_path().to_str().unwrap())
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write them to the respective file
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    // Link with the pmi to access its symbols. 
+    println!("cargo:rustc-link-search={}", src_lib_path.as_path().to_str().unwrap());
+    println!("cargo:rustc-link-lib=pmi2");
+}
