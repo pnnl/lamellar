b6666da6a6f0442bb1ad37ea080316d436ca21a2
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2025-10-12 19:15:00 -0700

    1. Added pmix as vendored dependency (Will clone pmix instead of looking for one in the system) 2. Added prrte-sys also as a vendored dependency 3. Both of these -sys crates will pull their -sys dependencies as vendored 4. Introduced a lamellar_main proceedural macro and crate to automatically launch distributed sessions.

--- build.rs
+++ build.rs
@@ -2,16 +2,37 @@
 extern crate bindgen;
 fn main(){
     let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let src_inc_path = std::path::PathBuf::from(std::env::var("PMIX_INC_DIR").expect("PMI2 not found. Please provide path to PMIX include dir in \"PMIX_INC_DIR\" environmental variable"));
-    let src_lib_path = std::path::PathBuf::from(std::env::var("PMIX_LIB_DIR").expect("PMIX not found. Please provide path to PMIX lib dir in \"PMIX_LIB_DIR\" environmental variable"));
+    let lib_event_dir = std::env::var("DEP_EVENT_ROOT").expect("Couldn't find libevent");
+    let libhwloc_dir = std::env::var("DEP_HWLOC_ROOT").expect("Couldn't find libevent");
 
+    let pmix_path = std::fs::canonicalize(std::path::PathBuf::from("openpmix")).unwrap();
+
+    println!("cargo:rerun-if-changed={}", "build.rs");
+    std::process::Command::new("./autogen.pl")
+        .current_dir(pmix_path.as_path())
+        .status()
+        .expect("Failed to autogen for PMIx");
+
+    let mut pmix_build = autotools::Config::new(pmix_path.as_path());
+    
+    pmix_build.reconf("-ivf");
+    pmix_build.disable_static();
+    pmix_build.enable_shared();
+    pmix_build.with("libevent", Some(&lib_event_dir));
+    pmix_build.with("hwloc", Some(&libhwloc_dir));
+    
+
+    let pmix_build = pmix_build.build();
+
+    let pmix_inc = pmix_build.join("include");
+    let pmix_lib = pmix_build.join("lib");
     // let header_path = src_inc_path.join("/pmi2.h");
     // let lib_path = src_lib_path.join("/lib/");
 
     // Generate the rust bindings
     let bindings = bindgen::Builder::default()
-        .header(src_inc_path.as_path().to_str().unwrap().to_string()+"/pmix.h")
-        .clang_arg(format!("-I{}",src_inc_path.as_path().to_str().unwrap()))
+        .header(pmix_inc.join("pmix.h").to_str().unwrap())
+        .clang_arg(format!("-I{}",pmix_inc.to_str().unwrap()))
         .generate()
         .expect("Unable to generate bindings");
 
@@ -21,6 +42,12 @@ fn main(){
         .expect("Couldn't write bindings!");
 
     // Link with the pmi to access its symbols. 
-    println!("cargo:rustc-link-search={}", src_lib_path.as_path().to_str().unwrap());
+    println!("cargo:rustc-link-search={}", pmix_lib.to_str().unwrap());
+    println!("cargo:rustc-link-search={}/lib", lib_event_dir);
+    println!("cargo:rustc-link-search={}/lib", libhwloc_dir);
+
     println!("cargo:rustc-link-lib=pmix");
+    println!("cargo:rustc-link-lib=static=event_core");
+    println!("cargo:rustc-link-lib=static=event_pthreads");
+    println!("cargo:rustc-link-lib=static=hwloc");
 }
