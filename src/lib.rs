11d7a65957e4e012d92ecbb1c28f5ebb2a7d5233
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2026-01-07 12:31:55 -0800

    [PMI] Added compile-time check that one of with-pmi1/2/x is enabled otherwise throws error

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#[cfg(not(any(
+    feature = "with-pmi1",
+    feature = "with-pmi2",
+    feature = "with-pmix"
+)))]
+compile_error!("At least one of the features 'with-pmi1', 'with-pmi2' or 'with-pmix' must be enabled");
 pub mod pmi;
 #[cfg(feature = "with-pmi1")]
 pub mod pmi1;
